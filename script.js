85ea616bfa1fa18060285d25739c4f18dbb9651f
guilhereimar <00001103538597sp@al.educacao.sp.gov.br>
2024-11-03 23:23:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+document.querySelectorAll('.btn-proximo');
+
+avanca.forEach()
+avanca.forEach(button => {
+    button.addEventListener()
+
+})
+const atual = document.querySelector('.ativo');
+const proximoPasso = 'passo-' + this.getAttribute()
